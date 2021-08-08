cd6ea5afda8d20996b4ddb0be74d72a02e847cb4
CoasterKaty <62079949+CoasterKaty@users.noreply.github.com>
2021-08-08 04:15:18 +0100

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<?php
+/* index.php Sample homepage for oAuth Demo project
+ *
+ * Katy Nicholson, last updated 08/08/2021
+ *
+ * https://github.com/CoasterKaty
+ * https://katytech.blog/
+ * https://twitter.com/coaster_katy
+ *
+ */
+
+
+// Load the auth module, this will redirect us to login if we aren't already logged in.
+include '../inc/auth.php';
+$Auth = new modAuth();
+
+//Display the username, logout link and a list of attributes returned by Azure AD.
+echo '<h1>Hello there, ' . $Auth->userName . '</h1>';
+echo '<h2><a href="/?action=logout">Log out</a></h2>';
+echo '<ul>';
+foreach ($Auth->userData as $item => $value) {
+        echo '<li>' . $item . ' = ' . $value . '</li>';
+}
+echo '</ul>';
+?>
+
